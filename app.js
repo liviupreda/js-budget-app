2241881a0b2bd1e110696e63b1d8c9366664f291
Liviu Preda <liviupreda@gmail.com>
2019-09-23 15:27:18 +0300

    read input field values, add Selector object in UICtrl

--- app.js
+++ app.js
@@ -3,24 +3,47 @@ const dataController = (function() {})();
 
 // -- UI CONTROLLER
 const UIController = (function() {
-  // code
+  // -- Selectors
+  const Selectors = {
+    inputType: document.querySelector('.add__type'),
+    inputDescription: document.querySelector('.add__description'),
+    inputValue: document.querySelector('.add__value'),
+    inputSubmitButton: document.querySelector('.add__btn')
+  };
+  return {
+    getInput: function() {
+      return {
+        // type is either inc or exp
+        type: Selectors.inputType.value,
+        description: Selectors.inputDescription.value,
+        value: Selectors.inputValue.value
+      };
+    },
+
+    getSelectors: function() {
+      return Selectors;
+    }
+  };
 })();
 
 // -- GLOBAL APP CONTROLLER
 const appController = (function(dataCtrl, UICtrl) {
+  const UISelectors = UICtrl.getSelectors();
+
   const appAddItem = function() {
     // 1. Get field input data
+    let input = UICtrl.getInput();
+    console.log(input);
+
     // 2. Add item to dataController
     // 3. Add item to UI
     // 4. Calculate budget
     // 5. Display budget in UI
-
-    console.log('ok');
   };
 
-  document.querySelector('.add__btn').addEventListener('click', appAddItem);
+  UISelectors.inputSubmitButton.addEventListener('click', appAddItem);
 
-  // Return key pressed event added to global document
+  // Return key pressed event in global document
   document.addEventListener('keyup', function(e) {
     if (e.keyCode === 13 || e.which === 13) {
       appAddItem();
